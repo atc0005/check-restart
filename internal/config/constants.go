076e8d1d0a9cbc7d2983126722a8e332318b71b9
Adam Chalkley <atc0005@users.noreply.github.com>
2022-11-01 04:45:04 -0500

    Fix incorrect short flag for show ignored option
    I was originally intending to name the flag `list-ignored` and
    changed my mind at the last minute. I missed updating this entry
    to reflect that change of direction.
    
    refs GH-32
    refs GH-46

--- internal/config/constants.go
+++ internal/config/constants.go
@@ -37,7 +37,7 @@ const (
 	TimeoutFlagLong      string = "timeout"
 	TimeoutFlagShort     string = "t"
 	ShowIgnoredFlagLong  string = "show-ignored"
-	ShowIgnoredFlagShort string = "li"
+	ShowIgnoredFlagShort string = "si"
 	LogLevelFlagLong     string = "log-level"
 	LogLevelFlagShort    string = "ll"
 )
